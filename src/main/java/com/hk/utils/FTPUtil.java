8e8fc7e29026c4d2b09e3bf1ba39b3fc3cb7c6d6
muyl <muylzh@163.com>
2018-11-02 21:41:34 +0800

    修改jdk

--- src/main/java/com/hk/utils/FTPUtil.java
+++ src/main/java/com/hk/utils/FTPUtil.java
@@ -4,13 +4,7 @@ package com.hk.utils;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 
 
 /**
